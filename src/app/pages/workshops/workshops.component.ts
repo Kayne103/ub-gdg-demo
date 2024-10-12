a5e1c29a10b8de0489b1d3caebaa1aba8663861e
kayne <kaenekarabolebakeng@gmail.com>
2024-10-12 12:55:02 +0200

    demo

---
+++ src/app/pages/workshops/workshops.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-workshops',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './workshops.component.html',
+  styleUrl: './workshops.component.css'
+})
+export class WorkshopsComponent {
+
+}
